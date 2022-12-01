90d7a0fc3c4d8383084731f380cc973dd51b5a11
“SrPatsu21” <paulosergiopierdona@gmail.com>
2022-12-01 12:36:56 -0300

    paginan para de carregar

--- Churras/server.js
+++ Churras/server.js
@@ -11,7 +11,7 @@ var pageHead =
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="Churras/FrontEnd/css/style.css">
 `
 var pageClose = 
 `
@@ -40,11 +40,12 @@ http.createServer(
         if (req.url == `/home`) {
             //setting vars
             var pageTitle = `
-            <title>${await getFile('Churras/title.txt')}</title>
+            <title>HOME</title>
             `;
 
             var pageBody = 
             `
+
             <header>${await getFile(`Churras/a.txt`)}</header>
             
             <main></main>
@@ -63,17 +64,30 @@ http.createServer(
             //send
             res.end();
 
-        } else if (req.url == `/main`) {
-
-        }else if (req.url == `/` || req.url == `` || req.url == null){
-            //title
-            var pageTitle = `<title>loading</title>`;
-            //page
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(pageTitle);
-            res.write(pageNull);
-            //send
-            res.end();
+        } else if (req.url == `/` || req.url == `` || req.url == null){
+                //setting vars
+                var pageTitle = `
+                <title>"NOT FOUND"</title>
+                `;
+    
+                var pageBody = 
+                `
+                <body>
+                <header>${await getFile(`Churras/FrontEnd/pageNull.html`)}</header>
+                <main></main>
+                <footer></footer>
+                `;
+    
+                //page
+                res.writeHead(200, {'Content-Type': 'text/html'});
+    
+                var page =  pageHead + pageTitle + pageBody + pageClose;
+    
+                //render page and file
+                res.write(page);
+    
+                //send
+                res.end();
         }
     }
 ).listen(port, ip);
