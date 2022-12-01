c51055506bf0d31eeda61eacd0baf5d630c2d026
“SrPatsu21” <paulosergiopierdona@gmail.com>
2022-12-01 10:53:24 -0300

    correcao basica

--- Churras/server.js
+++ Churras/server.js
@@ -11,16 +11,14 @@ var pageHead =
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-</head>
-<body>
+
 `
 var pageClose = 
 `
 </body>
 </html>
 `
-var pagenull = 
+var pageNull = 
 `
 nao a nada nessa pagina
 `
@@ -28,7 +26,6 @@ nao a nada nessa pagina
 const getFile = async filePath => {
     try {
       const data = await fs.promises.readFile(filePath, 'utf8')
-      console.log(data);
       return data
     }
     catch(err) {
@@ -40,15 +37,16 @@ const getFile = async filePath => {
 http.createServer(
     async function page(req, res){
 
-        //if (req.url == `/home`) {
+        if (req.url == `/home`) {
             //setting vars
-            var pageTitle = `<title>${await getFile('Churras/title.txt')}</title>`;
+            var pageTitle = `
+            <title>${await getFile('Churras/title.txt')}</title>
+            `;
 
             var pageBody = 
             `
             <header>${await getFile(`Churras/a.txt`)}</header>
             
-
             <main></main>
 
             <footer></footer>
@@ -60,32 +58,12 @@ http.createServer(
             var page =  pageHead + pageTitle + pageBody + pageClose;
 
             //render page and file
-            res.write(req.url);
             res.write(page);
 
             //send
             res.end();
 
-        /*} else if (req.url == `/main`) {
-            //title
-            var pageTitle = `<title>main</title>`;
-
-            //page
-            res.writeHead(200, {'Content-Type': 'text/html'});
-
-            //render file
-            res.write(pageHead);
-            res.write(pageTitle);
-            res.write(pageBody);
-            res.write(req.url);
-            res.write(`
-                <script>
-                $("header").load("a.txt");
-                </script>`);
-            res.write(pageClose);
-
-            //send
-            res.end();
+        } else if (req.url == `/main`) {
 
         }else if (req.url == `/` || req.url == `` || req.url == null){
             //title
@@ -93,10 +71,10 @@ http.createServer(
             //page
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(pageTitle);
-            res.write('no page');
+            res.write(pageNull);
             //send
             res.end();
-        }*/
+        }
     }
 ).listen(port, ip);
 
