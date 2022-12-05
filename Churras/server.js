d6c9955246efbf0f9767daeaf5dd7173fb5fe464
“SrPatsu21” <paulosergiopierdona@gmail.com>
2022-12-05 09:45:22 -0300

    funcionando

--- Churras/server.js
+++ Churras/server.js
@@ -3,7 +3,6 @@ var http = require(`http`);
 const port = 3729;
 const ip = `localhost`;
 const fs = require('fs');
-const { TIMEOUT } = require('dns');
 
 //setting functions
 const getFile = async filePath => {
@@ -16,32 +15,36 @@ const getFile = async filePath => {
       console.log(err);
     }
   }
-//calling parts
-const pageHead =
-`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Churras/FrontEnd/css/style.css">
-`;
 
 //create server
 http.createServer(
-    async function page(req, res){
+  async function page(req, res){
+      
+  //calling parts
+  const pageHead =
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="http://localhost:3729:/Churras/FrontEnd/css/style.css">
+    `;
+  const pageHeader = await getFile(`Churras/FrontEnd/pageNull.html`);
+  const pageFotter = await getFile(`Churras/FrontEnd/pageNull.html`);
+  const pageNull = await getFile(`Churras/FrontEnd/pageNull.html`);
+  const pageBody = await getFile(`Churras/FrontEnd/pageNull.html`);
+  const pageClose = `
+    </body>
+    </html>
+    `;
 
-      const pageHeader = await getFile(`Churras/FrontEnd/pageNull.html`);
-      const pageFotter = await getFile(`Churras/FrontEnd/pageNull.html`);
-        
         if (req.url == `/home`) {
-          try {
             //setting vars
             var pageTitle = `
             <title>HOME</title>
             `;
-            var pageBody = pageheader + pagemain + pagefooter;
 
             //page
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -51,16 +54,8 @@ http.createServer(
 
             //send
             res.end();
-            promise.all;
-          }
-          catch(err) {
-            console.log(err);
-          }
-
-                        
 
         } else if (req.url == `/` || req.url == `` || req.url == null){
-              try{
                 //setting vars
                 var pageTitle = `
                 <title>"NOT FOUND"</title>
@@ -70,14 +65,10 @@ http.createServer(
                 res.writeHead(200, {'Content-Type': 'text/html'});
         
                 //render page and file
-                res.write(pageHead + pageTitle + await getFile(`Churras/FrontEnd/pageNull.html`));
+                res.write(pageHead + pageTitle + pageNull);
     
                 //send
                 res.end();
-              }    
-              catch(err) {
-                console.log(err);
-              }
             }
         }
 ).listen(port, ip);
