106a5f88806e9d4664fa0cc9ac3087bd4c948df2
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-02-14 15:04:44 -0300

    teste realizado iniciarr outro

--- Churras/server teste.js
+++ Churras/server teste.js
@@ -1,77 +1,19 @@
 //setting vars
 var http = require(`http`);
-const fs = require(`fs`).promises;
-const port = 3729;
+const fs = require(`fs`);
+const port = 3000;
 const ip = `localhost`;
-
-//setting functions
-  const getFile = (dir) => {
-    return fs.readFile(dir);
-  }
+const door = process.env.PORT
 
 //create server
-http.createServer(
+const server = http.createServer((request, response)=>{
+    fs.readFile(`Churras/FrontEnd/pageHeader.html`,(err,arquivo)=>{
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(arquivo);
+      return response.end()
+    })
+})
 
-  async function page(request, response){
-      
-        //calling parts
-
-        const pageHead =
-          `
-          <!DOCTYPE html>
-          <html lang="en">
-          <head>
-              <meta charset="UTF-8">
-              <meta http-equiv="X-UA-Compatible" content="IE=edge">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <style type="text/css">`+ (await getFile(`Churras/FrontEnd/css/style.css`)) +`</style>
-          `;
-        const pageHeader = await getFile(`Churras/FrontEnd/pageHeader.html`);
-        const pageFooter = await getFile(`Churras/FrontEnd/pageFooter.html`);
-        const pageNull = await getFile(`Churras/FrontEnd/pageNull.html`);
-        const pageClose = `
-          </body>
-          </html>
-          `;
-
-          //criating pages
-          switch (request.url) {
-            case `/home`:
-                  //setting vars
-                  var pageTitle = `
-                  <title>HOME</title>
-                  `;
-                  let pageBody = await getFile(`Churras/FrontEnd/home.html`);
-
-                  //page
-                  response.writeHead(200, {'Content-Type': 'text/html'});
-
-                  //render page and file
-                  response.write(pageHead + pageTitle + pageHeader + pageBody + pageFooter + pageClose);
-
-                  //send
-                  response.end();
-                  break;  
-          
-            default:
-                  //setting vars
-                  var pageTitle = `
-                  <title>"NOT FOUND"</title>
-                  `;
-
-                  //page
-                  response.writeHead(200, {'Content-Type': 'text/html'});
-          
-                  //render page and file
-                  response.write(pageHead + pageTitle + pageNull);
-
-                  //send
-                  response.end();
-                  break;
-          }
-}
-
-).listen(port, ip);
+server.listen(door || port,()=>{console.log(`Server running...\nhttp://${ip}:${port}/home`)})
 
 //print out the HREF
-console.log(`Server running...\nhttp://${ip}:${port}/home`);
