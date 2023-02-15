bd08e365a58e438be6f5c5069d6b248d2b7123e4
“SrPatsu21” <paulosergiopierdona@gmail.com>
2023-02-14 21:02:50 -0300

    agora o css esta linkado

--- Churras/server.js
+++ Churras/server.js
@@ -2,8 +2,7 @@
 var http = require(`http`);
 const port = 3729;
 const ip = `localhost`;
-const fs = require(`fs`).promises;
-const door = process.env.PORT;
+var fs = require(`fs`).promises;
 
 //setting functions
   const getFile = (dir) => {
@@ -16,7 +15,9 @@ http.createServer(
       
         //calling parts
 
-        const pageHead =
+        let cssHref = `http://${ip}:${port}/Churras/FrontEnd/css/style.css`
+
+        let pageHead =
           `
           <!DOCTYPE html>
           <html lang="en">
@@ -24,8 +25,12 @@ http.createServer(
               <meta charset="UTF-8">
               <meta http-equiv="X-UA-Compatible" content="IE=edge">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <style type="text/css">`+ (await getFile(`Churras/FrontEnd/css/style.css`)) +`</style>
+              <link rel="stylesheet" href="${cssHref}">
+              <style>
+                body {min-width: 100vh;min-height: 100vh; margin: 0px; font-family: Verdana, Geneva, Tahoma, sans-serif;}
+              </style>
           `;
+
         const pageHeader = await getFile(`Churras/FrontEnd/pageHeader.html`);
         const pageFooter = await getFile(`Churras/FrontEnd/pageFooter.html`);
         const pageNull = await getFile(`Churras/FrontEnd/pageNull.html`);
@@ -36,23 +41,42 @@ http.createServer(
 
           //criating pages
           switch (request.url) {
-            case `/home`:
+                  
+            case`/Churras/FrontEnd/css/style.css`:
+
+                  //setting vars
+                  var pageTitle = `
+                  <title>Css</title>
+                  `;
+                  var pageMain = await getFile(`Churras/FrontEnd/css/style.css`);
+
+                  //page
+                  response.writeHead(200, {'Content-Type': 'text/css'});
+
+                  //render page and file
+                  response.write(pageTitle + pageMain);
+
+                  //send
+                  response.end();
+                  break;
+
+          case `/home`:
                   //setting vars
                   var pageTitle = `
                   <title>HOME</title>
                   `;
-                  let pageBody = await getFile(`Churras/FrontEnd/home.html`);
+                  var pageMain = await getFile(`Churras/FrontEnd/home.html`);
 
                   //page
                   response.writeHead(200, {'Content-Type': 'text/html'});
 
                   //render page and file
-                  response.write(pageHead + pageTitle + pageHeader + pageBody + pageFooter + pageClose);
+                  response.write(pageHead + pageTitle + pageHeader + pageMain + pageFooter + pageClose);
 
                   //send
                   response.end();
-                  break;  
-          
+                  break;
+              
             default:
                   //setting vars
                   var pageTitle = `
